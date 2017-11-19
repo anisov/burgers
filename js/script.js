a7570c79620208dd49b6d34cf9181a799e47c78b
Anisov <anisov.project@gmail.com>
2017-11-19 18:14:50 +0300

    #22 fixed all bugs in section comment, changed html/css for correct work

--- js/script.js
+++ js/script.js
@@ -120,3 +120,73 @@ if ( imgBlock .outerHeight() > textBlock.outerHeight()){
     var reqHeight = textBlock.outerHeight();
 }
 */
+
+//menu accordion
+
+const items = document.getElementsByClassName('js-open-menu'),
+    itemsText = document.getElementsByClassName('js-open-menu-text');
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', e => {
+        const item = items[i],
+            itemText = item.querySelector('.js-open-menu-text'),
+            activeItem = 'menu__elem--active',
+            activeText = 'menu__elem-text--active';
+
+        if (item.classList.contains(activeItem)){
+            item.classList.remove(activeItem);
+            itemText.classList.remove(activeText);
+        } else{
+            for (let j = 0; j < itemsText.length; j++) {
+                items[j].classList.remove(activeItem);
+                itemsText[j].classList.remove(activeText);
+            }
+            item.classList.add(activeItem);
+            itemText.classList.add(activeText);
+        }
+
+    });
+
+}
+
+
+
+
+
+// вопрос     //itemsText = items.getElementsByClassName('js-open-menu-text');
+/*
+    const $item = document.getElementsByClassName('js-menu-listen-click'),
+        activeItem = 'menu__elem--active';
+
+    for (let i = 0; i < $item.length; i++) {
+        $item[i].addEventListener('click', function () {
+            if (!(this.classList.contains(activeItem))) {
+                for (let j = 0; j < $item.length; j++) {
+                    $item[j].classList.remove(activeItem);
+                    this.classList.add(activeItem);
+                }
+            } else {
+                this.classList.remove(activeItem);
+            }
+        })
+    }
+
+
+        const container = document.getElementById('js-menu-listen-click');
+    container.addEventListener('click', e => {
+        let item = e.cu.firstChild(),
+            itemContainer = item.closest('.menu__elem'),
+            itemText = item.nextElementSibling,
+            activeItem = 'menu__elem--active',
+            activeText = 'menu__elem-text--active';
+        console.log(item)
+        if (itemContainer.classList.contains(activeItem)){
+
+            itemContainer.classList.remove(activeItem);
+            itemText.classList.remove(activeText);
+        } else{
+            itemContainer.classList.add(activeItem);
+            itemText.classList.add(activeText);
+        }
+
+    */
